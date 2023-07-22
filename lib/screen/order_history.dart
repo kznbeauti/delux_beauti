baa54fa8fabe87cb750598e0a0f9bc15f138d286
AungBoMyint <aungbomyint292@gmail.com>
2023-07-22 15:16:41 +0630

    user order history add id

--- lib/screen/order_history.dart
+++ lib/screen/order_history.dart
@@ -16,7 +16,9 @@ class OrderHistory extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black,),
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
         backgroundColor: appBarColor,
         elevation: 0,
         title: Padding(
@@ -32,120 +34,149 @@ class OrderHistory extends StatelessWidget {
               letterSpacing: 1,
             ),
           ),
-        ),),
+        ),
+      ),
       body: ValueListenableBuilder(
         valueListenable: Hive.box<HivePurchase>(purchaseBox).listenable(),
         builder: (context, Box<HivePurchase> box, widget) {
           return box.isNotEmpty
               ? SizedBox(
-            height: size.height,
-            width: size.width,
-            child: ListView(
-              children: box.values.map((purchase) {
-                return Obx(() {
-                  return Dismissible(
-                    key: Key(purchase.id),
-                    background: Container(
-                      color: Colors.black12,
-                    ),
-                    onDismissed: (direction) {
-                      box.delete(purchase.id);
-                    },
-                    direction: DismissDirection.startToEnd,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ExpansionPanelList(
-                          expansionCallback: (index, isExpanded) =>
-                              controller.setPurchaseId(purchase.id),
-                          children: [
-                            ExpansionPanel(
-                              isExpanded:
-                              controller.purchaseId == purchase.id,
-                              canTapOnHeader: true,
-                              headerBuilder: (context, isExpand) {
-
-                                return ListTile(
-                                  title: Text(
-                                    "မှာယူခဲ့သောအရေအတွက် = ${purchase.items.length}",
-                                    style: TextStyle(fontSize: 12),),
-                                  subtitle: Text(
-                                    "${purchase.totalPrice} ကျပ် "
-                                        "(ပို့ခ ${purchase.deliveryTownshipInfo[1]}ကျပ် ပေါင်းပြီး)",
-                                    style: TextStyle(fontSize: 12, color: Colors.black),),
-
-
-                                  trailing: Text(
-                                    "${purchase.dateTime.day}/"
-                                        "${purchase.dateTime.month}/"
-                                        "${purchase.dateTime.year}",
-                                    style: TextStyle(fontSize: 12),),
-
-                                );
-                              },
-                              body: SizedBox(
-                                height: purchase.items.length * 50,
-                                width: size.width * 0.8,
-                                child: ListView.builder(
-                                    padding: EdgeInsets.all(0),
-                                    itemCount: purchase.items.length,
-                                    itemBuilder: (_, o) {
-                                      final purchaseItem = purchase.items[o];
-                                      final itemPrice = purchaseItem.requirePoint! > 0 ? 0
-                                          : purchaseItem.discountPrice! > 0 ? purchaseItem.discountPrice : purchaseItem.price;
-                                      return Padding(
-                                        padding: const EdgeInsets.only(top: 5),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
+                  height: size.height,
+                  width: size.width,
+                  child: ListView(
+                    children: box.values.map((purchase) {
+                      return Obx(() {
+                        return Dismissible(
+                          key: Key(purchase.id),
+                          background: Container(
+                            color: Colors.black12,
+                          ),
+                          onDismissed: (direction) {
+                            box.delete(purchase.id);
+                          },
+                          direction: DismissDirection.startToEnd,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ExpansionPanelList(
+                                expansionCallback: (index, isExpanded) =>
+                                    controller.setPurchaseId(purchase.id),
+                                children: [
+                                  ExpansionPanel(
+                                    isExpanded:
+                                        controller.purchaseId == purchase.id,
+                                    canTapOnHeader: true,
+                                    headerBuilder: (context, isExpand) {
+                                      return ListTile(
+                                        title: Column(
                                           crossAxisAlignment:
-                                          CrossAxisAlignment.start,
+                                              CrossAxisAlignment.start,
                                           children: [
                                             Text(
-                                              "${o + 1}. ${purchase.items[o].itemName}",
-                                              style: TextStyle(fontSize: 12),
-                                            ),
-                                            SizedBox(
-                                              width: 25,
-                                            ),
-                                            Expanded(
-                                              child: Column(
-                                                crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                                children: [
-                                                  // Text(
-                                                  //   "${purchase.items[o].color  ?? "no color"}",
-                                                  //   style:
-                                                  //   TextStyle(fontSize: 10),
-                                                  // ),
-                                                  Text(
-                                                    "${purchase.items[o].size ?? "no size"}",
-                                                    style:
-                                                    TextStyle(fontSize: 12),
-                                                  )
-                                                ],
+                                              "ORDER ID:\n${purchase.id}\n",
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                color: Colors.grey.shade900,
                                               ),
                                             ),
                                             Text(
-                                              "$itemPrice x  ${purchase.items[o].count}",
-                                              style: TextStyle(fontSize: 12),
+                                              "မှာယူခဲ့သောအရေအတွက် = ${purchase.items.length}",
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                color: Colors.black,
+                                              ),
                                             ),
                                           ],
                                         ),
+                                        subtitle: Text(
+                                          "${purchase.totalPrice} ကျပ် "
+                                          "(ပို့ခ ${purchase.deliveryTownshipInfo[1]}ကျပ် ပေါင်းပြီး)",
+                                          style: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.black),
+                                        ),
+                                        trailing: Text(
+                                          "${purchase.dateTime.day}/"
+                                          "${purchase.dateTime.month}/"
+                                          "${purchase.dateTime.year}",
+                                          style: TextStyle(fontSize: 12),
+                                        ),
                                       );
-                                    }
-                                ),
-                              ),
-                            ),
-                          ]),
-                    ),
-                  );
-                });
-              }).toList(),
-            ),
-          )
+                                    },
+                                    body: SizedBox(
+                                      height: purchase.items.length * 50,
+                                      width: size.width * 0.8,
+                                      child: ListView.builder(
+                                          padding: EdgeInsets.all(0),
+                                          itemCount: purchase.items.length,
+                                          itemBuilder: (_, o) {
+                                            final purchaseItem =
+                                                purchase.items[o];
+                                            final itemPrice = purchaseItem
+                                                        .requirePoint! >
+                                                    0
+                                                ? 0
+                                                : purchaseItem.discountPrice! >
+                                                        0
+                                                    ? purchaseItem.discountPrice
+                                                    : purchaseItem.price;
+                                            return Padding(
+                                              padding:
+                                                  const EdgeInsets.only(top: 5),
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceBetween,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                    "${o + 1}. ${purchase.items[o].itemName}",
+                                                    style:
+                                                        TextStyle(fontSize: 12),
+                                                  ),
+                                                  SizedBox(
+                                                    width: 25,
+                                                  ),
+                                                  Expanded(
+                                                    child: Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        // Text(
+                                                        //   "${purchase.items[o].color  ?? "no color"}",
+                                                        //   style:
+                                                        //   TextStyle(fontSize: 10),
+                                                        // ),
+                                                        Text(
+                                                          "${purchase.items[o].size ?? "no size"}",
+                                                          style: TextStyle(
+                                                              fontSize: 12),
+                                                        )
+                                                      ],
+                                                    ),
+                                                  ),
+                                                  Text(
+                                                    "$itemPrice x  ${purchase.items[o].count}",
+                                                    style:
+                                                        TextStyle(fontSize: 12),
+                                                  ),
+                                                ],
+                                              ),
+                                            );
+                                          }),
+                                    ),
+                                  ),
+                                ]),
+                          ),
+                        );
+                      });
+                    }).toList(),
+                  ),
+                )
               : Center(
-            child: Text("No order history."),
-          );
+                  child: Text("No order history."),
+                );
         },
       ),
     );
